173c3a095a7d5e14283a21cd2bfdace0c48d7f33
AnnRoy27 <annroy2708@gmail.com>
2020-12-03 19:25:02 +0530

    First Commit

--- Itravel/src/test/java/Test/LoginTest.java
+++ Itravel/src/test/java/Test/LoginTest.java
@@ -1,10 +1,13 @@
 package Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LoginTest {
   @Test
   public void f() {
+	  WebDriver driver =new ChromeDriver();
 	  System.out.println("hello page");
   }
 }
